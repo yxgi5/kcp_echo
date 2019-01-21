d77db9d5c3a6c61b3e7989099341ad4964b60d2e
Andreas Zhang <denglitsch@gmail.com>
2019-01-21 10:48:35 +0800

    一次发送并ack

--- udp_client.c
+++ udp_client.c
@@ -59,7 +59,7 @@ uint32_t iclock()
 }
 
 #define MAXLINE 1024
-
+#define LOCAL_PORT    8001
 #define SERV_PORT 8000
 
 int fd = 0;
@@ -72,8 +72,9 @@ int udp_output(const char *buf, int len, ikcpcb *kcp, void *user)
 
 int main(int argc, char *argv[])
 {
-    struct sockaddr_in servaddr;
+    struct sockaddr_in servaddr,cliaddr;
     int sockfd, n;
+    int ret;
     char buf[MAXLINE];
     //char str[INET_ADDRSTRLEN];
     socklen_t servaddr_len;
@@ -85,11 +86,43 @@ int main(int argc, char *argv[])
         fprintf(stderr, "socket falied\n");
         exit(EXIT_FAILURE);
     }
+    
+    
+    // set socket non-blocking
+    {
+        int flags = fcntl(sockfd, F_GETFL, 0);
+        if (flags == -1)
+        {
+            //std::cerr << "get socket non-blocking: fcntl error return with errno: " << errno << " " << strerror(errno) << std::endl;
+            fprintf(stderr, "get socket non-blocking err\n");
+        }
+        int ret = fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+        if (ret == -1)
+        {
+            //std::cerr << "set socket non-blocking: fcntl error return with errno: " << errno << " " << strerror(errno) << std::endl;
+            fprintf(stderr, "set socket non-blocking err\n");
+		}
+    }
+
+    
+    //为套接字绑定本地地址信息
+    cliaddr.sin_family = AF_INET;
+    cliaddr.sin_port = htons(LOCAL_PORT);
+    cliaddr.sin_addr.s_addr = 0;
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+    if ((ret = bind(sockfd, (struct sockaddr* )&cliaddr, addrlen)) < 0)
+    {
+        fprintf(stderr, "bind falied\n");
+        exit(EXIT_FAILURE);
+    }
 
     servaddr_len = sizeof(struct sockaddr_in);
     memset(&servaddr, 0, sizeof(servaddr));
+    
+    //为套接字指定目的地址信息
     servaddr.sin_family = AF_INET;
-    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+    //inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servaddr.sin_port = htons(SERV_PORT);
     dstAddr = &servaddr;
     
@@ -120,7 +153,26 @@ int main(int argc, char *argv[])
         {
             IUINT32 ts1 = iclock();
             ikcp_update(kcp1, ts1);
+			n = recvfrom(sockfd, buf, MAXLINE, 0, (struct sockaddr *)&servaddr, &servaddr_len);
+			if (n>0)
+        	{
+            	ikcp_input(kcp1, buf, n);
+        	}
+
+            int msgLen = ikcp_peeksize(kcp1);
+		    while (msgLen > 0)
+		    {
+			    memset(buf, 0, MAXLINE);
+			    if (msgLen > 0)
+			    {
+				    ikcp_recv(kcp1, buf, msgLen);
+                    fputs(buf, stdout);
+			    }
+			    msgLen = ikcp_peeksize(kcp1);
+		    }
+            //break;
         }
+        printf("hah\n");
     }
     close(sockfd);
     return 0;
--- udp_server.c
+++ udp_server.c
@@ -3,7 +3,6 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <string.h>
@@ -11,8 +10,8 @@
 #include <time.h>
 #include <fcntl.h>
 #include <errno.h>
-
 #include "ikcp.h"
+
 
 void millisecond_sleep(size_t n_millisecond)
 {
@@ -60,39 +59,123 @@ uint32_t iclock()
 {
     return (uint32_t)(iclock64() & 0xfffffffful);
 }
+
 #define MAXLINE 1024
-#define SERV_PORT 8000
-int main(void)
+#define LOCAL_PORT    8000
+#define SERV_PORT 8001
+
+
+int fd = 0;
+struct sockaddr* dstAddr = NULL;
+int udp_output(const char *buf, int len, ikcpcb *kcp, void *user)
 {
-    struct sockaddr_in servaddr, cliaddr;
-    socklen_t cliaddr_len;
-    int sockfd;
+	sendto(fd, buf, len, 0, dstAddr, sizeof(*dstAddr));
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in servaddr,cliaddr;
+    int sockfd, n;
+    int ret;
     char buf[MAXLINE];
-    char str[INET_ADDRSTRLEN];
-    int n;
+    //char str[INET_ADDRSTRLEN];
+    socklen_t cliaddr_len;
 
+    //创建套接字
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(SERV_PORT);
-
-    bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-
-    printf("Accepting connections... \n");
-    while(1)
+    if (sockfd < 0)
     {
-	    memset(buf, 0, MAXLINE);  // 清空数组
-        cliaddr_len = sizeof(cliaddr);
-        n = recvfrom(sockfd, buf, MAXLINE, 0, (struct sockaddr *)&cliaddr, &cliaddr_len);
-        if (n == -1)
+        fprintf(stderr, "socket falied\n");
+        exit(EXIT_FAILURE);
+    }
+    fd = sockfd;
+    
+    // set socket non-blocking
+    {
+        int flags = fcntl(sockfd, F_GETFL, 0);
+        if (flags == -1)
         {
-            perror("recvfrom error");
-	}
+            //std::cerr << "get socket non-blocking: fcntl error return with errno: " << errno << " " << strerror(errno) << std::endl;
+            fprintf(stderr, "get socket non-blocking err\n");
+        }
+        int ret = fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+        if (ret == -1)
+        {
+            //std::cerr << "set socket non-blocking: fcntl error return with errno: " << errno << " " << strerror(errno) << std::endl;
+            fprintf(stderr, "set socket non-blocking err\n");
+		}
+    }
+
+    
+    //为套接字绑定本地地址信息
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(LOCAL_PORT);
+    servaddr.sin_addr.s_addr = 0;
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+    if ((ret = bind(sockfd, (struct sockaddr* )&servaddr, addrlen)) < 0)
+    {
+        fprintf(stderr, "bind falied\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&cliaddr, 0, sizeof(cliaddr));
+    
+    //为套接字指定目的地址信息
+    cliaddr.sin_family = AF_INET;
+    //inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+    cliaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    cliaddr.sin_port = htons(8001);
+    cliaddr_len = sizeof(struct sockaddr_in);
+    //if ((ret = connect(sockfd, (struct sockaddr* )&cliaddr, cliaddr_len)) < 0)
+    //{
+    //    fprintf(stderr, "connect falied\n");
+    //    exit(EXIT_FAILURE);
+    //}
+    dstAddr = &cliaddr;
+
+    ikcpcb *kcp1 = ikcp_create(0x11223344, (void*)0);
+    kcp1->output = udp_output;
+
+    while(1)
+    //while(fgets(buf, MAXLINE, stdin) != NULL)
+    {
+        IUINT32 ts1 = iclock();
+        ikcp_update(kcp1, ts1);
+	    memset(buf, 0, MAXLINE);  // 清空数组
+        //cliaddr_len = sizeof(cliaddr);
+        n = recvfrom(sockfd, buf, MAXLINE, 0, (struct sockaddr *)&cliaddr, &cliaddr_len);
+        //n = recv(sockfd, buf, MAXLINE, 0);
+        //if (n == -1)
+        //{
+            //perror("recv error");
+        //}
+        if (n>0)
+        {
+            ikcp_input(kcp1, buf, n);
+        }
         //buf[n] = '\0';
         //printf("%s", buf);
-        fputs(buf, stdout);
+        //fputs(buf, stdout);
+
+
+        int msgLen = ikcp_peeksize(kcp1);
+		while (msgLen > 0)
+		{
+			memset(buf, 0, MAXLINE);
+			if (msgLen > 0)
+			{
+				ikcp_recv(kcp1, buf, msgLen);
+                fputs(buf, stdout);
+			}
+			msgLen = ikcp_peeksize(kcp1);
+		}
+
+
+
+
+
         //printf("received from %s at PORT %d\n", inet_ntop(AF_INET, &cliaddr.sin_addr, str, (socklen_t )sizeof(str)), ntohs(cliaddr.sin_port));
         //for (i = 0; i < n; i++)
         //    buf[i] = toupper(buf[i]);
